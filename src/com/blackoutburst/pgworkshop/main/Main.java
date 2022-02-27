d38a69a05c970422dd5c1fe9f9d7465a9c62539d
Blackoutburst <blackoutburst@hotmail.com>
2022-02-27 17:44:41 +0100

    Teleport the player to the spawnpoint on join

--- src/com/blackoutburst/pgworkshop/main/Main.java
+++ src/com/blackoutburst/pgworkshop/main/Main.java
@@ -1,13 +1,25 @@
 package com.blackoutburst.pgworkshop.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
+    public static Location spawn;
+    public static boolean gameRunning;
+
     @Override
     public void onEnable() {
+        spawn = new Location(Bukkit.getWorlds().get(0), -1774.5f, 40, 739.5f);
+    }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().teleport(spawn);
     }
 
 }
