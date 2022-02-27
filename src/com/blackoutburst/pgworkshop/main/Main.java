fd6d376c457f4b40144163ecaf0241435a7e0a81
Blackoutburst <blackoutburst@hotmail.com>
2022-02-27 17:30:30 +0100

    Added plugin template

---
+++ src/com/blackoutburst/pgworkshop/main/Main.java
@@ -0,0 +1,14 @@
+package com.blackoutburst.pgworkshop.main;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
+
