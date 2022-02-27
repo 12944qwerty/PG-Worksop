3857a61ca630ee362c27122c1ad5841f7398c067
Blackoutburst <blackoutburst@hotmail.com>
2022-02-27 17:45:52 +0100

    Cancelled extra event when the game is running

--- src/com/blackoutburst/pgworkshop/main/Main.java
+++ src/com/blackoutburst/pgworkshop/main/Main.java
@@ -4,13 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
     public static Location spawn;
-    public static boolean gameRunning;
+    public static boolean gameRunning = false;
 
     @Override
     public void onEnable() {
@@ -22,5 +25,19 @@ public class Main extends JavaPlugin implements Listener {
         event.getPlayer().teleport(spawn);
     }
 
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        event.setCancelled(gameRunning);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        event.setCancelled(gameRunning);
+    }
 }
 
